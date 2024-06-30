cda0357110bb7e7ee163fec06debf07e132e4f0e
diego <diego.oliveira@bornlogic.com>
2024-06-30 16:52:58 -0300

    update

--- curriculum/assets/php/getCurriculum.php
+++ curriculum/assets/php/getCurriculum.php
@@ -8,27 +8,20 @@ function FindCurricullum($id) {
     $sqlResumes = "
     SELECT *
     FROM resumes r
+    WHERE r.id = $integer
     ORDER BY r.id DESC";
     $resultResumes = $conn->query($sqlResumes);
 
     $sqlExperiences = "
     SELECT *
     FROM experiences e 
-    LEFT JOIN resumes r ON r.id = e.resume_id
+    LEFT JOIN resumes r ON r.id = e.resume_id AND r.id = $integer
     ORDER BY r.id DESC";
     $resultExperiences = $conn->query($sqlExperiences);
-
-
-    $dataExperiences = array();
-
-    while ($rowExperiences = $resultExperiences->fetch_assoc()) {
-        $dataExperiences[] = $rowExperiences;
-    }
 
     if ($resultResumes->num_rows > 0) {
         $curriculo = $resultResumes->fetch_assoc().firs
         $experiences = [];
-
 
         while ($row = $resultExperiences->fetch_assoc()) {
             $experiences[] = $row;
